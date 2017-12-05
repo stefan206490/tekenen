7f1f80bab6765cbcba28b6d5dd1bf7aa7a2d76ab
Stefan Beckers <stefan.beckers206490@student.leeuwenborgh.nl>
2017-12-05 18:48:57 +0100

    you can see an image and delete the image

--- tekenen/MainWindow.xaml.cs
+++ tekenen/MainWindow.xaml.cs
@@ -20,9 +20,72 @@ namespace tekenen
     /// </summary>
     public partial class MainWindow : Window
     {
+        public Point point1;
+        public Point point2;
+        public Point point3;
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void btnPoligon_Click(object sender, RoutedEventArgs e)
+        {
+            SolidColorBrush DarkBlueBrush = new SolidColorBrush();
+            DarkBlueBrush.Color = Colors.DarkBlue;
+            SolidColorBrush GreenBrush = new SolidColorBrush();
+            GreenBrush.Color = Colors.Green;
+
+            Polygon Mypoligon = new Polygon();
+            Mypoligon.Fill = GreenBrush;
+            Mypoligon.Stroke = DarkBlueBrush;
+            Mypoligon.StrokeThickness = 5;
+
+            Point point1 = new Point(50, 100);
+            Point point2 = new Point(200, 100);
+            Point point3 = new Point(200, 200);
+            PointCollection polygonPoints = new PointCollection();
+            polygonPoints.Add(point1);
+            polygonPoints.Add(point2);
+            polygonPoints.Add(point3);
+
+            Mypoligon.Points = polygonPoints;
+
+            draw.Children.Add(Mypoligon);
+        }
+
+        private void btnSquare_Click(object sender, RoutedEventArgs e)
+        {
+            Rectangle Myrectangle = new Rectangle();
+            Myrectangle.Stroke = Brushes.Black;
+            Myrectangle.Fill = Brushes.Blue;
+            Myrectangle.Height = 200;
+            Myrectangle.Width = 200;
+            Myrectangle.RadiusX = 50;
+            Myrectangle.RadiusY = 50;
+            draw.Children.Add(Myrectangle);
+        }
+
+        private void btnLine_Click(object sender, RoutedEventArgs e)
+        {
+            Line Line1 = new Line();
+            Line1.X1 = 100;
+            Line1.Y1 = 100;
+            Line1.X2 = 200;
+            Line1.Y2 = 200;
+            Line1.Stroke = Brushes.Black;
+            Line1.StrokeThickness = 5;
+            draw.Children.Add(Line1);
+        }
+
+        private void btnClear_Click(object sender, RoutedEventArgs e)
+        {
+            draw.Children.Clear();
+            scroll.Visibility = Visibility.Hidden;
+        }
+
+        private void btnimage_Click(object sender, RoutedEventArgs e)
+        {
+            scroll.Visibility = Visibility.Visible;
         }
     }
 }
